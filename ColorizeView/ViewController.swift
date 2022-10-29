88859a55be5cf0ae195569d6f6ac54ed626982f4
Hunyakamolodoy <dmitriy_seleznev_01@mail.ru>
2022-10-30 01:28:23 +0700

    final

--- ColorizeView/ViewController.swift
+++ ColorizeView/ViewController.swift
@@ -10,8 +10,6 @@ import UIKit
 class ViewController: UIViewController {
 
 // MARK: - IB Outlets
-    
-    
     @IBOutlet var mainView: UIView!
     
     @IBOutlet var redLabel: UILabel!
@@ -22,9 +20,6 @@ class ViewController: UIViewController {
     @IBOutlet var greenSlider: UISlider!
     @IBOutlet var blueSlider: UISlider!
     
-    
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -34,55 +29,45 @@ class ViewController: UIViewController {
         setupBlueSlider()
         setupGreenSlider()
         
-        changeColor()
-        
-        
-        
-        
+        colorizeView()
     }
     
 // MARK: - IB Actions
     @IBAction func redSliderAction() {
         redLabel.text = String(format: "%.2f", redSlider.value)
-        changeColor()
+        colorizeView()
     }
     
     @IBAction func greenSliderAction() {
         greenLabel.text = String(format: "%.2f", greenSlider.value)
-        changeColor()
+        colorizeView()
     }
     
     @IBAction func blueSliderAction() {
-        //blueLabel.text = (round(blueSlider.value * 100) / 100).formatted()
-        //blueLabel.text = blueSlider.value.rounded().formatted()
         blueLabel.text = String(format: "%.2f", blueSlider.value)
-        changeColor()
+        colorizeView()
     }
-    
-    
     
 // MARK: - Private Methods
     private func setupRedSlider() {
-        redSlider.value = 0
+        redSlider.value = 0.44
         redSlider.minimumValue = 0
         redSlider.maximumValue = 1
     }
     
     private func setupGreenSlider() {
-        greenSlider.value = 0
+        greenSlider.value = 0.44
         greenSlider.minimumValue = 0
         greenSlider.maximumValue = 1
     }
     
     private func setupBlueSlider() {
-        blueSlider.value = 0
+        blueSlider.value = 0.44
         blueSlider.minimumValue = 0
         blueSlider.maximumValue = 1
     }
     
-    private func changeColor() {
+    private func colorizeView() {
         mainView.backgroundColor = UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1)
     }
-    
 }
-
